969173d4d05890cc65ced405c631db29f94f22bb
limonzishad <zishadlimon@gmail.com>
2022-04-18 20:38:16 +0600

    fake data updated

--- src/components/Service/Service.js
+++ src/components/Service/Service.js
@@ -4,15 +4,16 @@ import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Service = (props) => {
-    const { name, details, img } = props.service;
+    const { name, details, img, price } = props.service;
     return (
         <div>
             <Card className="component-shadow">
                 <div className="cards-container">
                     <img src={img} alt={name} />
-                    <h5>{name}</h5>
+                    <h5 className="mt-4">{name}</h5>
                 </div>
                 <p className="service-text">{details}</p>
+                <p className="service-text">Price: {price}</p>
                 <div><Link to="/enroll"><Button variant="primary" className="w-100 mx-auto">ENROLL</Button></Link></div>
             </Card>
         </div>
