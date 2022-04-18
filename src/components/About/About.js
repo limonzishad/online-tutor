0bb6e3f9de058d6be9a84fd6e9e97e981eb8bc6b
limonzishad <zishadlimon@gmail.com>
2022-04-18 22:21:47 +0600

    about section updated

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,8 +8,13 @@ const About = () => {
         <Card body className="about">
             <h1>TUTOR'S INFORMATION</h1>
             <hr></hr>
-            <p>This assignment is assigned by programing hero.</p>
-            <Button><Link as={Link} to="/login">LOGIN</Link></Button>
+            <div className="w-25 mx-auto">
+                <img src="https://www.vhv.rs/dpng/d/526-5267211_gentleman-png-download-formal-profile-picture-png-transparent.png" alt="" />
+            </div>
+            <p>Deiago Andres Rodrigo Roderick Gonzalez</p>
+            <p>MSc. in Computer Science</p>
+            <p>BSc. in Computer Science and Engineering</p>
+            <p>Web Development Instructor at online-tutor.com</p>
         </Card>
     );
 }
